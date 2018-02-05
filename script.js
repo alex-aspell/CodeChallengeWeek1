e79cfe83bdcd04fab18f88b10fce89fcdbf7a9a0
Alex Aspell <alexgaspell@gmail.com>
2018-02-05 14:53:40 -0600

    swap and delete buttons sorta work

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 $(document).ready(onReady);
-
+var clicks = 0;
 function onReady(){
   $('#generateButton').on('click', function(){
-    var newDiv = '<div>';
-    newDiv += '<p></p>';
+    function clicker(){
+      var click = clicks++;
+    }
+    var newDiv = '<div class=goingToBeRed>';
+    newDiv += '<p class="clickCounter">Clicks: </p>';
+    newDiv +='<button class=swapButton>Swap</button>';
+    newDiv +='<button class=deleteButton>Delete</button>';
     newDiv +='</div>';
     $('#allWork').last().append(newDiv);
     console.log('button clicked');
+
+    $('.goingToBeRed').css('color','red');
+    $('.swapButton').on('click', function(){
+      $('.goingToBeRed').css('color', 'yellow');
+    })
+    $('.deleteButton').on('click', function(){
+      $('.goingToBeRed').empty()
+    })
   })
 }
